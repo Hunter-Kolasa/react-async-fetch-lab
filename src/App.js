194b7f722100336357e0135804f8ddae98d0dbc4
Hunter <kolasa.hunter@aol.com>
2021-04-04 13:41:04 -0400

    commit tree 6dc0f8ff2302700189b3ce3c5e0c82e09ca6fc8e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component {
+    state = {
+        peopleInSpace: []
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState({
+                    peopleInSpace: data.people
+                })
+            })
+
+    }
+    render() {
+        return (
+            <div>{this.state}</div>
+        )
+    }
+
+}
+export default App
